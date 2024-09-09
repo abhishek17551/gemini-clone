91331d60dad9949209c285941a4ed98f88f5d207
abhishek17551 <ababhishek113@gmail.com>
2024-09-09 17:28:51 +0530

    added styles & functionalities

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './sidebar.css'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { BsPlusSquareFill } from "react-icons/bs";
+import { RiMessage2Fill } from "react-icons/ri";
+import { BsFillPatchQuestionFill } from "react-icons/bs";
+import { FaHistory } from "react-icons/fa";
+import { IoSettingsOutline } from "react-icons/io5";
+
 
 const Sidebar = () => {
+    const [extended,setExtended] = useState()
   return (
     <div className="sidebar">
+        <div className="top">
+            <div className='menu'><GiHamburgerMenu onClick={() => setExtended((prev) => !prev)}/></div>
+            <div className="new-chat">
+                <BsPlusSquareFill/>
+                {extended ? <p>New Chat</p> : null} 
+            </div>
+            {
+                extended ? 
+                <div className='recent'>
+                    <p className='recent-title'>Recent</p>
+                    <div className="recent-entry">
+                        <RiMessage2Fill/>
+                        <p>What is React?</p>
+                    </div>
+                </div> : null
+
+            }
+        </div>
+        <div className="bottom">
+            <div className="bottom-item recent-entry">
+                <BsFillPatchQuestionFill/>
+                {extended ? <p>Help</p> : null}
+            </div>
+            <div className="bottom-item recent-entry">
+                <FaHistory/>
+                {extended ? <p>Activity</p> : null}
+            </div>
+            <div className="bottom-item recent-entry">
+                <IoSettingsOutline/>
+                {extended ? <p>Settings</p> : null}
+            </div>
+        </div>
 
     </div>
+    
   )
 }
 
